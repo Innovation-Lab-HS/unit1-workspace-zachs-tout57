bf49f887bfa6075a4262aad39b1c0cc5a7d83b38
zachs-tout57 <163046020+zachs-tout57@users.noreply.github.com>
2024-09-13 18:19:54 +0000

    stuff

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+  // all code in Java sits inside a class (woah ive only made like 3 classes before)
+
+  // ever Java program needs a public main method
+  public static void main(String[] args) {
+    // for the next while, ALL code will use main()
+    System.out.println("Hello.");
+    System.out.println("wowie printing stuff " + 1);
+  }
+}
